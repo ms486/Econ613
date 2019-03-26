4e9378bc590ddd5316010496a894febbb2a43bf9
ms486 <sidibe.modibo@gmail.com>
2019-03-26 09:58:18 -0500

    march26

--- Slides/PanelData/Codes/Applications.R
+++ Slides/PanelData/Codes/Applications.R
@@ -55,3 +55,75 @@ fm33 <- lm(log(violent) ~ law + prisoners + density + income +
 printCoefmat(coeftest(fm33, vcov = sandwich)[1:9,])
 
 texreg(list(fm31,fm32,fm33))
+
+#========================
+# application 2
+# empl UK
+#========================
+
+data("EmplUK", package="plm")
+dat = EmplUK
+
+stargazer(dat)
+
+spec1 = lm(log(emp)~log(wage)+log(capital)+log(output),data=dat)
+spec2 = lm(emp~wage+capital+output,data=dat)
+spec4 = pggls(emp~wage+capital+as.factor(sector), data=EmplUK, model="pooling")
+
+summary(spec2)
+summary(spec1)
+summary(spec3)
+summary(spec4)
+
+texreg::texreg(list(spec1,spec2))
+
+spec4 = pggls(log(emp)~log(wage)+log(capital), data=EmplUK, model="within")
+spec3 = pggls(log(emp)~log(wage)+log(capital), data=EmplUK, model="pooling")
+
+summary(spec3)
+summary(spec4)
+
+stargazer::stargazer(spec3,spec4)
+
+#========================
+# application 3
+# Us States production
+#========================
+
+data("Produc", package="plm")
+
+formula = log(gsp) ~ log(pcap) + log(pc) + log(emp) + unemp
+
+model1 = plm(formula, data=Produc, model="within")
+model2 = plm(formula, data=Produc, model="between")
+model3 = plm(formula, data=Produc, model="fd")
+
+texreg::texreg(list(model1,model2,model3))
+
+
+#========================
+# application 4
+# estimating fixed effects 
+#========================
+
+set.seed(123)
+
+ni   = 10000
+nt   = 10
+nvar = 3
+
+alph = runif(ni,-10,10)
+
+ymat = mat.or.vec(ni,nt)
+xmat = array(runif(ni*nt*nvar),c(ni,nt,nvar))
+bt   = runif(3)
+
+for (i in 1:nt)
+{
+  ymat[,i] = alph + bt[1]*xmat[,i,1] + bt[2]*xmat[,i,2] + bt[3]*xmat[,i,3] + rnorm(ni)
+}
+  
+dat = data.frame(yy=c(ymat),ind=rep(1:ni,nt),time=rep(1:nt,ni),x1=c(xmat[,,1]),x2=c(xmat[,,2]),x3=c(xmat[,,3]))
+
+reg1 = lm(yy~x1+factor(ind),data=dat)
+
