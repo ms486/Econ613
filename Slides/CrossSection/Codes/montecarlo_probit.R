8c3719bf4e6fe908351855ca3b146737d2219747
ms486 <ms486@duke.edu>
2021-02-15 10:58:43 -0500

    Update montecarlo_probit.R

--- Slides/CrossSection/Codes/montecarlo_probit.R
+++ Slides/CrossSection/Codes/montecarlo_probit.R
@@ -13,6 +13,7 @@ yhat = true_par[1] + true_par[2]*x1 + true_par[3]*x2 + true_par[4]*x3 + rnorm(no
 # simulate yhat
 yvar = as.numeric(yhat>0)
 
+table(yvar)
 
 #==========================
 # probit in R 
@@ -20,7 +21,6 @@ yvar = as.numeric(yhat>0)
 
 reg1 = glm(yvar~x1+x2+x3,family = binomial(link = "probit"))
 summary(reg1)
-
 
 #==========================
 # Programming the likelihood
@@ -30,6 +30,7 @@ flike = function(par,x1,x2,x3,yvar)
 {
   xbeta           = par[1] + par[2]*x1 + par[3]*x2 + par[4]*x3
   pr              = pnorm(xbeta)
+#  pr              = exp(beta)/(1+exp(beta)) logit
   pr[pr>0.999999] = 0.999999
   pr[pr<0.000001] = 0.000001
   like           = yvar*log(pr) + (1-yvar)*log(1-pr)
@@ -37,10 +38,21 @@ flike = function(par,x1,x2,x3,yvar)
 }
 
 # test if the function is correct
-test_par = est[,1]
+test_par = reg1$coefficients
 flike(test_par,x1,x2,x3,yvar)
 logLik(reg1)
 
+ntry = 100
+out = mat.or.vec(ntry,4)
+for (i0 in 1:ntry)
+{
+start    = runif(4,-10,10)
+#res      = optim(start,fn=flike,method="BFGS",control=list(trace=6,REPORT=1,maxit=1000),x1=x1,x2=x2,x3=x3,yvar=yvar)
+res      = optim(start,fn=flike,method="BFGS",control=list(trace=6,maxit=1000),x1=x1,x2=x2,x3=x3,yvar=yvar)
+out[i0,] = res$par
+}
+
+#===========================================
 start = runif(4)
 res  = optim(start,fn=flike,method="BFGS",control=list(trace=6,REPORT=1,maxit=1000),x1=x1,x2=x2,x3=x3,yvar=yvar,hessian=TRUE)
 
